14f65578fcea0a3fb39eb8447b1c2dfa2256807b
Cody Mercer <me@codymercer.com>
2016-08-13 21:13:54 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -29,6 +29,80 @@ function plugin(Vue) {
             return events;
         }
     });
+
+    Vue.mixin({
+
+        beforeCreate() {
+
+            if (typeof this.$options.events != 'object') return;
+
+            this.$on('hook:mounted', () => {
+
+                for (var key in this.$options.events) {
+                    let cb = this.$options.events[key];
+
+                    console.log(cb.arguments);
+
+                    events.on(key, cb.apply(this));
+                }
+            });
+        }
+
+    });
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(plugin);
+}
+
+export default plugin;
+function plugin(Vue) {
+
+    if (plugin.installed) {
+        return;
+    }
+
+    const events = new Vue({
+        methods: {
+            fire(name, data = null) {
+                this.emit(name, data)
+            },
+
+            emit(name, data = null) {
+                this.$emit(name, data);
+            },
+
+            listen(name, cb) {
+                this.on(name, cb)
+            },
+
+            on(name, cb) {
+                this.$on(name, cb);
+            }
+        }
+    });
+
+    Object.defineProperty(Vue.prototype, '$events', {
+        get() {
+            return events;
+        }
+    });
+
+    Vue.mixin({
+
+        beforeCreate() {
+
+            if (typeof this.$options.events != 'object') return;
+
+            this.$on('hook:mounted', () => {
+
+                for (var key in this.$options.events) {
+                    events.on(key, this.$options.events[key]);
+                }
+            });
+        }
+
+    });
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
