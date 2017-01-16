93748ec8028db614bb6365e168452d526c9c60a4
LancerComet <chw644@hotmail.com>
2017-01-16 22:25:28 +0800

    + Add types for vue-events.

---
+++ types/events.ts
@@ -0,0 +1,64 @@
+/**
+ * Vue-Events types.
+ */
+
+import Vue = require('vue')
+import { ComponentOptions, PluginFunction } from "vue"
+
+export declare class VueEvents {
+  static install: PluginFunction<never>
+
+  /**
+   * Fire a event.
+   *
+   * @param {string} eventName Name of your registered event.
+   * @param {any} value The data passed to the function.
+   * @memberOf VueEvents
+   */
+  fire(eventName: string, value: any): void
+
+  /**
+   * Fire a event.
+   *
+   * @param {string} eventName Name of your registered event.
+   * @param {any} value The data passed to the function.
+   * @memberOf VueEvents
+   */
+  emit(eventName: string, value: any): void
+
+  /**
+   * Fire a event.
+   *
+   * @param {string} eventName Name of your registered event.
+   * @param {any} value The data passed to the function.
+   * @memberOf VueEvents
+   */
+  $emit(eventName: string, value: any): void
+
+  /**
+   * Register a new event.
+   *
+   * @param {string} eventName Name for this new event.
+   * @param {Function} callback Function that you want.
+   * @memberOf VueEvents
+   */
+  listen(eventName: string, callback: Function): void
+
+  /**
+   * Register a new event.
+   *
+   * @param {string} eventName Name for this new event.
+   * @param {Function} callback Function that you want.
+   * @memberOf VueEvents
+   */
+  on(eventName: string, callback: Function): void
+
+  /**
+   * Register a new event.
+   *
+   * @param {string} eventName Name for this new event.
+   * @param {Function} callback Function that you want.
+   * @memberOf VueEvents
+   */
+  $on(eventName: string, callback: Function): void
+}
