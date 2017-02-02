9e8a4dc6c09b99fd87d955aaf3032276f1d21557
Cody Mercer <me@codymercer.com>
2017-02-02 09:53:05 -0600

    large refactor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,115 @@
-import events from './event-bus'
-
 function plugin(Vue) {
 
-    if (plugin.installed) return
+  // Exit if the plugin has already been installed.
+  if (plugin.installed) return
 
-    Object.defineProperty(Vue.prototype, '$events', {
-        get() {
-            return events
-        }
-    });
+  // Create a `vm` to serve as our global event bus.
+  const events = new Vue({
+    methods: {
+      /**
+       * Emit the given event.
+       *
+       * @param {string|object} event
+       * @param {...*} args
+       */
+      emit(event, ...args) {
+        this.$emit(event, ...args)
+      },
+
+      /**
+       * Emit the given event.
+       *
+       * @param {string|object} event
+       * @param {...*} args
+       */
+      fire(event, ...args) {
+        this.emit(event, ...args)
+      },
+
+      /**
+       * Listen for the given event.
+       *
+       * @param {string} event
+       * @param {function} callback
+       */
+      on(event, callback) {
+        this.$on(event, callback)
+      },
+
+      /**
+       * Listen for the given event.
+       *
+       * @param {string} event
+       * @param {function} callback
+       */
+      listen(event, callback) {
+        this.on(event, callback)
+      },
+
+      /**
+       * Listen for the given event once.
+       *
+       * @param {string} event
+       * @param {function} callback
+       */
+      once(event, callback) {
+        this.$once(event, callback)
+      },
+
+      /**
+       * Remove one or more event listeners.
+       *
+       * @param {string} event
+       * @param {function} callback
+       */
+      off(event, callback) {
+        this.$off(event, callback)
+      },
+
+      /**
+       * Remove one or more event listeners.
+       *
+       * @param {string} event
+       * @param {function} callback
+       */
+      remove(event, callback) {
+        this.off(event, callback)
+      }
+    }
+  })
+
+  // Extend `Vue.prototype` to include our global event bus.
+  Object.defineProperty(Vue.prototype, '$events', {
+    get() {
+      return events
+    }
+  })
+
+  // Determine if we're dealing with a Vue 2.0 installation.
+  if (typeof Vue.prototype._mount === 'function') {
+    // Register a mixin that adds an `events` option to Vue 2.0 components.
+    Vue.mixin({
+      // Hook into the Vue 2.0 `beforeCreate` life-cycle event.
+      beforeCreate() {
+        // Exit if there's no `events` option.
+        if (typeof this.$options.events !== 'object') return
+        // Listen for the `hook:beforeMount` Vue 2.0 life-cycle event.
+        this.$on('hook:beforeMount', () => {
+          // Loop through each event.
+          for (var key in this.$options.events) {
+            // Register a listener for the event.
+            events.$on(key, this.$options.events[key].bind(this))
+          }
+        })
+      }
+    })
+  }
 }
 
+// Check for `window.Vue`
 if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(plugin)
+  // Install plugin automatically.
+  window.Vue.use(plugin)
 }
 
 export default plugin
